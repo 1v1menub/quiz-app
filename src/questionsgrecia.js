74a20093776363144ca3d22bbe6adbbcae8de20a
1v1menub <ignacio.rubio@utec.edu.pe>
2023-02-17 10:25:51 -0500

    .

--- src/questionsgrecia.js
+++ src/questionsgrecia.js
@@ -6,12 +6,15 @@ const questionsraw = {
             correct_answer: "Tiburón",
             incorrect_answers: ["Delfín", "Orca", "Tigre"],
             question: "¿Cuál es mi animal favorito?",
-            correct_msg: "Tan bello y peligroso como tu."
+            correct_msg: "Tan bello y peligroso como tú.",
+            incorrect_msg: "Au, si no te acordaste de esta, te deseo mucha suerte."
         },
         {
             correct_answer: "2",
             incorrect_answers: ["1", "5", "3"],
-            question: "¿Cuántos hermanos tengo?"
+            question: "¿Cuántos hermanos tengo?",
+            correct_msg: "Tan bello y peligroso como tú.",
+            incorrect_msg: "Au, si no te acordaste de esta, te deseo mucha suerte."
         }
     ]
 }
